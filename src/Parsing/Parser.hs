00263ee42531a8cddfb2830e9722f8c0de978bf2
Arraying <paul.huebner@googlemail.com>
2023-01-10 14:29:00 +0100

    Parse generic binary operations.

--- src/Parsing/Parser.hs
+++ src/Parsing/Parser.hs
@@ -25,7 +25,17 @@ parseSExpr (SSym "Nil") = Right NilExt
 parseSExpr (SSym "True") = Right TrueExt
 parseSExpr (SSym "False") = Right FalseExt
 parseSExpr (SList [SSym "+", l, r]) = binop "+" l r
-parseSExpr _ = Left $ FWSyntaxError "Not implemented or syntax error"
+parseSExpr (SList [SSym "-", l, r]) = binop "-" l r
+parseSExpr (SList [SSym "*", l, r]) = binop "*" l r
+parseSExpr (SList [SSym "/", l, r]) = binop "/" l r
+parseSExpr (SList [SSym "&&", l, r]) = binop "&&" l r
+parseSExpr (SList [SSym "||", l, r]) = binop "||" l r
+parseSExpr (SList [SSym "==", l, r]) = binop "==" l r
+parseSExpr (SList [SSym ">", l, r]) = binop ">" l r
+parseSExpr (SList [SSym "<", l, r]) = binop "<" l r
+parseSExpr (SList [SSym "if", c, t, f]) = Left $ FWSyntaxError "Not implemented" 
+parseSExpr (SList (SSym "list" : xs)) = Left $ FWSyntaxError "Not implemented" 
+parseSExpr _ = Left $ FWSyntaxError "Syntax error"
 
 binop :: String -> SExpr -> SExpr -> Either FWError ExprExt
 binop s l r = mapRight (\(l', r') -> Right $ BinOpExt s l' r') $ mapBin parseSExpr (l, r)
