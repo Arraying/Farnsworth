df5734bc3b49fdf7f4718d728e6654eb535e23ba
Arraying <paul.huebner@googlemail.com>
2023-01-16 08:01:56 +0100

    Convert desugarer to Monads.

--- src/Desugaring/Desugarer.hs
+++ src/Desugaring/Desugarer.hs
@@ -14,16 +14,31 @@ desugar NilExt          = Right NilC
 desugar TrueExt         = Right TrueC
 desugar FalseExt        = Right FalseC
 desugar (IdExt s)       = Right $ IdC s
-desugar (UnOpExt "-" e) = mapRight (\e' -> Right $ NegC e') $ desugar e
-desugar (BinOpExt "cons" l r) = mapRight (\(l', r') -> Right $ ConsC l' r') $ mapBin desugar (l, r)
-desugar (IfExt c t f) = case mapMany desugar [c, t, f] of
-  Left e             -> Left e
-  Right [c', t', f'] -> Right $ IfC c' t' f'
-  Right _            -> Left $ FWDesugError "Internal if-statement error"
-desugar (ListExt xs) = mapRight (\xs' -> Right $ list xs') $ mapMany desugar xs
-desugar (AnonFnExt a b) = mapRight (\b' -> Right $ curryLambda a b') $ desugar b
-desugar (NamedFnExt s a b) = mapRight (\b' -> namedLambda s $ curryLambda a b') $ desugar b
-desugar (AppExt x xs) = mapRight (\x' -> mapRight (\xs' -> Right $ curryApplication x' xs') $ mapMany desugar xs) $ desugar x
+desugar (UnOpExt "-" e) = do
+  e' <- desugar e
+  Right $ NegC e'
+desugar (BinOpExt "cons" l r) = do
+  l' <- desugar l
+  r' <- desugar r
+  Right $ ConsC l' r'
+desugar (IfExt c t f) = do
+  c' <- desugar c
+  t' <- desugar t
+  f' <- desugar f
+  Right $ IfC c' t' f'
+desugar (ListExt xs) = do
+  xs' <- mapMany desugar xs
+  Right $ list xs'
+desugar (AnonFnExt a b) = do
+  b' <- desugar b
+  Right $ curryLambda a b'
+desugar (NamedFnExt s a b) = do
+  c <- curryLambda a <$> desugar b
+  namedLambda s c
+desugar (AppExt x xs) = do
+  x' <- desugar x
+  xs' <- mapMany desugar xs
+  Right $ curryApplication x' xs'
 desugar e               = Left $ FWDesugError $ show e
 
 list :: [ExprC] -> ExprC
